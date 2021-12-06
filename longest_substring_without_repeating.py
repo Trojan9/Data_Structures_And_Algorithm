8019450a47b933aea96532434d37249daac8209e
unknown <Banjo Oluwatimilehin Emmanuel>
2021-12-06 10:50:48 +0100

    longest_substring

---
+++ longest_substring_without_repeating.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+        #using sliding window method
+        #https://www.youtube.com/watch?v=wiGpQwVHdE0
+        getmax=0
+        hashset=set()
+        l=0
+        for i in range(len(s)):
+            while s[i] in hashset:
+                #this will remove the first value of the hashmap
+                #as there is no removeAt function
+                hashset.remove(s[l])
+                l+=1
+            hashset.add(s[i])
+            getmax=max(getmax,len(hashset))
+            
+        return getmax 
+            
+        
