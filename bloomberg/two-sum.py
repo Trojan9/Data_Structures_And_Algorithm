ca4c631a7a3569f1521265c0053cd11dc3a9af37
OLUWATIMILEHIN BANJO <TIMMYBANJO@GMAIL.COM>
2023-08-18 11:52:20 +0100

    2sum

--- bloomberg/two-sum.py
+++ bloomberg/two-sum.py
@@ -9,3 +9,26 @@ class Solution(object):
             if ((target-nums[i]) in nums) and (nums.index(target-nums[i])!=i) :
                 return [i,nums.index(target-nums[i])]
         
+
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # make an empty map
+        prevMap={}  #val:index
+        # get index and val
+        for i,n in enumerate(nums):
+            diff=target-n
+            # from here we will mostly get our result when we get to the second value
+            # [1,2,3,4]...target=4....we will get our result at 3.....4-3=1, by then 1 has been added to the map, 
+            # we will return index of 1 and 3
+            if diff in prevMap:
+                return [prevMap[diff],i]
+            prevMap[n]=i
+        return 
+        
