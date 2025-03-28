d7e204935c576c881c8379d090d6baaff206b878
Trojan <54397120+Trojan9@users.noreply.github.com>
2025-03-28 18:11:24 +0000

    Update valid-anagram.py

--- bloomberg/valid-anagram.py
+++ bloomberg/valid-anagram.py
@@ -17,4 +17,25 @@ class Solution:
                 return False
         #means it passes this return true
         return True
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #create a map with each letter count...if the maps are same, then it's an anagram
+        s_dict = {} 
+        t_dict = {}
+        
+        for letter in s:
+            if letter in s_dict:
+                s_dict[letter] +=1
+            else:  
+                s_dict[letter] =1 
+                
+        for letter in t:
+            if letter in t_dict:
+                t_dict[letter] +=1
+            else:  
+                t_dict[letter] =1 
+        
+        return s_dict == t_dict    
             
