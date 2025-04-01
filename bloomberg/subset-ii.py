1bb74c78679c5b78653885eae20149962d2831a7
Trojan <54397120+Trojan9@users.noreply.github.com>
2025-04-01 17:21:17 +0100

    Update subset-ii.py

--- bloomberg/subset-ii.py
+++ bloomberg/subset-ii.py
@@ -1,6 +1,9 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         sub=[[]]
+#Because sorting helps group duplicates together, and that makes it easier to avoid generating duplicate subsets.
+#[1, 2, 2]
+#Then while building subsets, you'll always encounter the duplicate 2s next to each other, so it's easier to detect and prevent duplicates like [2, 1] and [1, 2] showing up twice in different forms.
         nums = sorted(nums)
         for element in nums:
             #javascript way
