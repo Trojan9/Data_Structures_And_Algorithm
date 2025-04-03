fbeb144d3bb74cb4825c2547d5e65beb1ba36397
Trojan <54397120+Trojan9@users.noreply.github.com>
2025-04-03 02:13:20 +0100

    Update remove-all-adjacent-duplicates-in-string-ii.py

--- bloomberg/remove-all-adjacent-duplicates-in-string-ii.py
+++ bloomberg/remove-all-adjacent-duplicates-in-string-ii.py
@@ -19,5 +19,18 @@ class Solution(object):
             res +=(char*count)
             
         return res
-            
+
+
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack = []
+
+        for i in range(len(s)):
+            if stack and stack[-1][0] == s[i]:
+                stack[-1][1]+=1
+                if stack[-1][1] == k:
+                    stack.pop()
+            else:
+                stack.append([s[i],1])
+        return ''.join(k * v for k, v in stack)
         
