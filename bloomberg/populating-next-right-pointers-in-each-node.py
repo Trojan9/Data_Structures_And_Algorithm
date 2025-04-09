f85dd752ad3dca22c4e6719f58b3b69d627dfb98
Trojan <54397120+Trojan9@users.noreply.github.com>
2025-04-09 01:16:09 +0100

    Update populating-next-right-pointers-in-each-node.py

--- bloomberg/populating-next-right-pointers-in-each-node.py
+++ bloomberg/populating-next-right-pointers-in-each-node.py
@@ -26,3 +26,47 @@ class Solution:
         dfs(root,0)
         #just return root
         return root
+
+########## Another SOlution , better for me ############# 
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        #If the tree is empty, return None immediately (base case).
+        if not root:
+            return None
+        #This initializes a queue (from collections.deque) with the root node.
+        #We use this queue for level-order traversal (BFS).
+        queue = deque([root])
+        #Continue the BFS loop while there are nodes in the queue.
+        while queue:
+            #Get the number of nodes in the current level.
+            #We use this to know where the level starts and ends.
+            size = len(queue)
+            #Loop through all nodes at the current level.
+            #q is the current node being processed, removed from the front of the queue.
+            for i in range(size):
+                q= queue.popleft()
+                #if it is not the end of the breath we equal next to the next in the breath line
+                #If q is not the last node on the level, set its .next to the next node in the queue (i.e., its    right neighbor).
+
+                #queue[0] is safe here because we’re still inside the level.
+                if i < size -1:
+                    q.next = queue[0]
+                #Add the left and right children of the current node to the queue.
+                #This prepares the next level for processing.
+                if q.left:
+                    queue.append(q.left)
+                if q.right:
+                    queue.append(q.right)
+        #After setting all .next pointers, return the root of the updated tree.        
+        return root
