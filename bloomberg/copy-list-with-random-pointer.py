8726f8788e56edcd9b483a3a287213280eb417fb
Trojan <54397120+Trojan9@users.noreply.github.com>
2025-04-09 02:36:58 +0100

    Update copy-list-with-random-pointer.py

--- bloomberg/copy-list-with-random-pointer.py
+++ bloomberg/copy-list-with-random-pointer.py
@@ -30,5 +30,46 @@ class Solution:
             cur=cur.next
         #then return the duplicated list
         return oldToCopy[head]#because head is the key to the duplcate head
+
+
+
+########### Another solution, i prefer this ##############
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+
+        old_to_new = {}
+
+        current = head
+
+        while current:
+            old_to_new[current] = Node(current.val)
+            current = current.next
+        
+        current = head
+
+        while current:
+            old_to_new[current].next = old_to_new.get(current.next)
+            old_to_new[current].random = old_to_new.get(current.random)
+            current = current.next
+        
+        return old_to_new[head]
+
+#✅ Strategy: Hash Map to Track Originals → Copies
+#Use a dictionary to map original node → copied node
+
+#First pass: create all copied nodes (with just .val)
+
+Second pass: assign .next and .random using the map
             
             
